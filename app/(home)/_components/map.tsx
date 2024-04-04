51fdcb0336c263d009bedb160e8998882ac0e4fd
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-04-05 04:55:14 +0530

    reduced reduncy and fixed ssr

--- app/(home)/_components/map.tsx
+++ app/(home)/_components/map.tsx
@@ -5,11 +5,9 @@ import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import L, { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-const BasicMap = () => {
-  const [initialPosition, setInitial] = useState({ lat: 22.1696, lng: 91.4996 });
-  const [destinationPosition, setDestination] = useState({ lat: 22.2637, lng: 91.7159 });
+const BasicMap = ({ initialPosition, destinationPosition }: { initialPosition: {lat: number, lng: number}, destinationPosition: {lat: number, lng: number} }) => {
   const [shipPosition, setShipPosition] = useState(initialPosition);
-  const [speed, setSpeed] = useState(20);
+  const [speed ] = useState(20);
 
   const [reachedDes, setIsReached] = useState(false);
 
@@ -43,26 +41,6 @@ const BasicMap = () => {
     }, interval);
   };
 
-  const handleInputChange = (event: { target: { name: any; value: any; }; }, positionType: string) => {
-    const { name, value } = event.target;
-    if (positionType === 'initial') {
-        setInitial(prevState => ({
-            ...prevState,
-            [name]: parseFloat(value)
-        }));
-    } else if (positionType === 'destination') {
-        setDestination(prevState => ({
-            ...prevState,
-            [name]: parseFloat(value)
-        }));
-    }
-  };
-
-  const changeSpeed = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newSpeed = parseInt(event.target.value);
-    setSpeed(newSpeed);
-  };
-
   const resetMovement = () => {
     setIsReached(false); 
     setShipPosition({ lat: 22.16996, lng: 91.4996 });
@@ -93,79 +71,15 @@ const BasicMap = () => {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      
-      <div className="flex gap-3 mb-5 w-[121vh]">
-        <div className="flex border-[2px] p-4 rounded-lg w-full justify-between items-space">
-          <div className="flex flex-col gap-y-1">
-            <p className="font-bold">Starting</p>
-            <div className="flex gap-[7px] mt-7 items-center">
-                <p className="font-bold w-10">Lat:</p>
-                <input
-                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
-                    placeholder="Enter the Latitude"
-                    onChange={(event) => handleInputChange(event, 'initial')}
-                    defaultValue={initialPosition.lat}
-                    name="lat"
-                />
-            </div>
-            <div className="flex gap-[7px]">
-                <p className="font-bold w-10">Long:</p>
-                <input
-                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
-                    placeholder="Enter the Longitude"
-                    onChange={(event) => handleInputChange(event, 'initial')}
-                    defaultValue={initialPosition.lng}
-                    name="lng"
-                />
-            </div>
-          </div>
-          <div className="flex text-[blue]">
-            <div className="flex gap-[7px] items-center">
-                <p className="font-bold">Speed:</p>
-                <input
-                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
-                    placeholder="Enter the speed"
-                    onChange={ (event) => changeSpeed(event) }
-                    defaultValue={speed}
-                    name="speed"
-                />
-            </div>
-          </div>
-          <div className="flex flex-col gap-y-1">
-            <p className="font-bold">Ending</p>
-            <div className="flex gap-[7px] items-center mt-7">
-                <p className="font-bold w-11">Lat:</p>
-                <input
-                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
-                    placeholder="Enter the Latitude"
-                    onChange={(event) => handleInputChange(event, 'destination')}
-                    defaultValue={destinationPosition.lat}
-                    name="lat"
-                />
-            </div>
-            <div className="flex gap-[7px] items-center">
-                <p className="font-bold">Long:</p>
-                <input
-                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
-                    placeholder="Enter the Longitude"
-                    onChange={(event) => handleInputChange(event, 'destination')}
-                    defaultValue={destinationPosition.lng}
-                    name="lng"
-                />
-            </div>
-          </div>
-        </div>
-        {reachedDes ? (
-          <button className="border rounded-lg bg-red-400 px-4 py-1 w-[11.2rem]" onClick={resetMovement}>
-            Reset
-          </button>
-        ) : (
-          <button className="border rounded-lg bg-[#46ec46] px-4 py-1 w-[11.2rem] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" onClick={startMovement}>
-            Start
-          </button>
-        )}
-      </div>
-
+      {reachedDes ? (
+        <button className="border rounded-lg bg-red-400 px-4 py-1 w-[11.2 rem]" onClick={resetMovement}>
+          Reset
+        </button>
+      ) : (
+        <button className="border rounded-lg bg-[#46ec46] px-4 py-1 w-[11.2rem] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" onClick={startMovement}>
+          Start
+        </button>
+      )}
       <MapContainer center={center} zoom={11}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,113 @@
 "use client"
 
-import BasicMap from "./_components/map";
+// import BasicMap from "./_components/map";
+import React, { useState } from "react";
+import dynamic from 'next/dynamic';
+
+const BasicMap = dynamic(() => import('./_components/map'), {ssr: false})
 
 export default function Home() {
 
+  const [initialPosition, setInitial] = useState({ lat: 22.1696, lng: 91.4996 });
+  const [destinationPosition, setDestination] = useState({ lat: 22.2637, lng: 91.7159 });
+  const [speed, setSpeed] = useState(20);
+
+  const handleInputChange = (event: { target: { name: any; value: any; }; }, positionType: string) => {
+    const { name, value } = event.target;
+    if (positionType === 'initial') {
+        setInitial(prevState => ({
+            ...prevState,
+            [name]: parseFloat(value)
+        }));
+    } else if (positionType === 'destination') {
+        setDestination(prevState => ({
+            ...prevState,
+            [name]: parseFloat(value)
+        }));
+    }
+  };
+
+  const changeSpeed = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSpeed = parseInt(event.target.value);
+    setSpeed(newSpeed);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
-      <BasicMap />
+
+       <div className="flex gap-3 mb-5 w-[121vh]">
+        <div className="flex border-[2px] p-4 rounded-lg w-full justify-between items-space">
+          <div className="flex flex-col gap-y-1">
+            <p className="font-bold">Starting</p>
+            <div className="flex gap-[7px] mt-7 items-center">
+                <p className="font-bold w-10">Lat:</p>
+                <input
+                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
+                    placeholder="Enter the Latitude"
+                    onChange={(event) => handleInputChange(event, 'initial')}
+                    defaultValue={initialPosition.lat}
+                    name="lat"
+                />
+            </div>
+            <div className="flex gap-[7px]">
+                <p className="font-bold w-10">Long:</p>
+                <input
+                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
+                    placeholder="Enter the Longitude"
+                    onChange={(event) => handleInputChange(event, 'initial')}
+                    defaultValue={initialPosition.lng}
+                    name="lng"
+                />
+            </div>
+          </div>
+          <div className="flex text-[blue]">
+            <div className="flex gap-[7px] items-center">
+                <p className="font-bold">Speed:</p>
+                <input
+                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
+                    placeholder="Enter the speed"
+                    onChange={ (event) => changeSpeed(event) }
+                    defaultValue={speed}
+                    name="speed"
+                />
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-1">
+            <p className="font-bold">Ending</p>
+            <div className="flex gap-[7px] items-center mt-7">
+                <p className="font-bold w-11">Lat:</p>
+                <input
+                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
+                    placeholder="Enter the Latitude"
+                    onChange={(event) => handleInputChange(event, 'destination')}
+                    defaultValue={destinationPosition.lat}
+                    name="lat"
+                />
+            </div>
+            <div className="flex gap-[7px] items-center">
+                <p className="font-bold">Long:</p>
+                <input
+                    className="border rounded-lg px-2 max-w-[7rem] h-[35px] focus:outline-none"
+                    placeholder="Enter the Longitude"
+                    onChange={(event) => handleInputChange(event, 'destination')}
+                    defaultValue={destinationPosition.lng}
+                    name="lng"
+                />
+            </div>
+          </div>
+        </div> 
+         {/* {reachedDes ? (
+          <button className="border rounded-lg bg-red-400 px-4 py-1 w-[11.2 rem]" onClick={resetMovement}>
+            Reset
+          </button>
+        ) : (
+          <button className="border rounded-lg bg-[#46ec46] px-4 py-1 w-[11.2rem] shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" onClick={startMovement}>
+            Start
+          </button>
+        )} */}
+      </div> 
+
+      <BasicMap initialPosition={initialPosition} destinationPosition={destinationPosition} />
     </main>
   );
 }
